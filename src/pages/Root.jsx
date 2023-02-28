f194ced5f75fdf76606b43231f47a6e9b7600c1f
housamhegazy <geohousam@gmail.com>
2023-03-01 00:37:52 +0300

    nnnn

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import { Avatar, Link } from "@mui/material";
+import {AppBar,Toolbar,Typography,Avatar, Link } from "@mui/material";
 export default function Root() {
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
-          <Link sx={{ flexGrow: 1 }} underline="none" color="inherit" href="/">
+          <Link
+            sx={{ flexGrow: 1,transition:".3s", "&:hover": { color: "red" ,fontSize:'22px' } }}
+            underline="none"
+            color="inherit"
+            href="/"
+          >
             my expenses
           </Link>
-          <Typography mx="10px" color="inherit" variant="body1">
+          <Typography mx={"10px"} color="inherit" variant="body1">
             housam hegazy
           </Typography>
           <Avatar
             variant="circular"
             alt="Remy Sharp"
-            src="http://mui.com/static/images/avatar/1.jpg"
+            src="./images/housam.jpg"
           />
         </Toolbar>
       </AppBar>
