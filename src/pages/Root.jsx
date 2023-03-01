b3b7de0850a2adb7c4694d16d27947251f94e6af
housamhegazy <geohousam@gmail.com>
2023-03-02 01:15:24 +0300

    box

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -2,18 +2,29 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Appbar from "MUI Components/AppBar";
 import Drawerr from "MUI Components/Drawer";
+import { Box } from "@mui/system";
+import Home from "./home/Home";
 
 const drawerWidth = 240;
 
 export default function Root() {
   return (
     <div>
-      <Appbar drawerWidth={drawerWidth}/>
-      {/* drawer */}  
-      <Drawerr drawerWidth={drawerWidth}/>
-      <Outlet />
+      <Appbar drawerWidth={drawerWidth} />
+      {/* drawer */}
+      <Drawerr drawerWidth={drawerWidth} />
+
+      {/* here we put everythin in page  */}
+      <Box
+        sx={{
+          ml: `${drawerWidth}px`,
+          display: "flex",
+          justifyContent: "center",
+        }}
+        className="border"
+      >
+        <Outlet />
+      </Box>
     </div>
-
-
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Home.css';
 function Home() {
-  return (
-  <h1>home</h1>
+  return (<div>
+  
+  <h1>homevvvvvvvvvvvvvvvvv</h1>
+  <h1>hohomevvvvvvvvvvvvvvvvv</h1>
+  <h1>homevvvvvvvvvvvvvvvvv</h1>
+  </div>
   );
 }
 
